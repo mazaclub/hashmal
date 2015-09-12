2114826e9f85256d7a138b131d9f5e51a31fab13
Kefkius <kefkius@mail.com>
2015-09-12 12:17:06 -0400

    List packages instead of py_modules in setup.py

--- setup.py
+++ setup.py
@@ -24,30 +24,9 @@ setup(
         'Topic :: Software Development :: Interpreters'
     ],
     scripts=['hashmal'],
-    py_modules=[
-        'hashmal_lib.__init__',
-        'hashmal_lib.config',
-        'hashmal_lib.dock_handler',
-        'hashmal_lib.gui_utils',
-        'hashmal_lib.help_widgets',
-        'hashmal_lib.main_window',
-        'hashmal_lib.script_widget',
-        'hashmal_lib.scriptedit',
-        'hashmal_lib.settings_dialog',
-        'hashmal_lib.tx_widget',
-        'hashmal_lib.core.__init__',
-        'hashmal_lib.core.my_config',
-        'hashmal_lib.core.script',
-        'hashmal_lib.core.stack',
-        'hashmal_lib.core.utils',
-        'hashmal_lib.docks.__init__',
-        'hashmal_lib.docks.addr_encoder',
-        'hashmal_lib.docks.base',
-        'hashmal_lib.docks.blockchain',
-        'hashmal_lib.docks.script_gen',
-        'hashmal_lib.docks.stack',
-        'hashmal_lib.docks.tx_builder',
-        'hashmal_lib.docks.tx_deserializer',
-        'hashmal_lib.docks.variables'
+    packages=[
+        'hashmal_lib',
+        'hashmal_lib.core',
+        'hashmal_lib.docks'
     ]
 )

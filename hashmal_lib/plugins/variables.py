cda83d929de2e4172602fa71c074c4b295707801
Kefkius <kefkius@mail.com>
2015-09-17 15:17:09 -0400

    Variables: Use custom model

--- hashmal_lib/plugins/variables.py
+++ hashmal_lib/plugins/variables.py
@@ -6,10 +6,95 @@ from PyQt4.QtGui import *
 from PyQt4 import QtCore
 
 from base import BaseDock, Plugin
-from hashmal_lib.gui_utils import floated_buttons
+from hashmal_lib.gui_utils import floated_buttons, HBox
 
 def make_plugin():
     return Plugin([Variables])
+
+class VarsModel(QtCore.QAbstractTableModel):
+    """Model for stored variables."""
+    def __init__(self, data, parent=None):
+        super(VarsModel, self).__init__(parent)
+        self.vars_data = data
+
+    def columnCount(self, parent = QtCore.QModelIndex()):
+        return 2
+
+    def rowCount(self, parent = QtCore.QModelIndex()):
+        return len(self.vars_data)
+
+    def headerData(self, section, orientation, role = QtCore.Qt.DisplayRole):
+        if orientation == QtCore.Qt.Vertical:
+            return QtCore.QVariant(None)
+
+        data = None
+        if section == 0:
+            if role == QtCore.Qt.DisplayRole:
+                data = 'Key'
+            elif role == QtCore.Qt.ToolTipRole:
+                data = 'Variable name'
+        elif section == 1:
+            if role == QtCore.Qt.DisplayRole:
+                data = 'Value'
+            elif role == QtCore.Qt.ToolTipRole:
+                data = 'Variable value'
+
+        return QtCore.QVariant(data)
+
+    def flags(self, index):
+        return (QtCore.Qt.ItemIsSelectable |
+                QtCore.Qt.ItemIsEnabled)
+
+    def dataAt(self, row, column, role = QtCore.Qt.DisplayRole):
+        index = self.createIndex(row, column)
+        return self.data(index, role)
+
+    def data(self, index, role = QtCore.Qt.DisplayRole):
+        if role != QtCore.Qt.DisplayRole and role != QtCore.Qt.UserRole:
+            return QtCore.QVariant(None)
+        data = None
+        r = index.row()
+        c = index.column()
+        key = self.vars_data.keys()[r]
+
+        if c == 0:
+            data = key
+        elif c == 1:
+            if role == QtCore.Qt.DisplayRole:
+                data = self.vars_data[key]
+            elif role == QtCore.Qt.UserRole:
+                data = self.classify_data(self.vars_data[key])
+
+        return QtCore.QVariant(data)
+
+    def classify_data(self, value):
+        """Determine what to categorize a value as."""
+        # If the value is not hex, assume text
+        try:
+            i = int(value, 16)
+        except ValueError:
+            return 'Text'
+
+        # See if it's a raw transaction.
+        try:
+            t = CTransaction.deserialize(value.decode('hex'))
+            return 'Raw Transaction'
+        except Exception:
+            pass
+
+        # Use the generic 'Hex' category if nothing else matches.
+        return 'Hex'
+
+    def set_key(self, key, value):
+        self.beginInsertRows( QtCore.QModelIndex(), self.rowCount(), self.rowCount() )
+        self.vars_data[key] = value
+        self.endInsertRows()
+
+    def remove_key(self, key):
+        row = self.vars_data.keys().index(key)
+        self.beginRemoveRows( QtCore.QModelIndex(), row, row )
+        del self.vars_data[key]
+        self.endRemoveRows()
 
 class Variables(BaseDock):
     def __init__(self, handler):
@@ -31,38 +116,38 @@ class Variables(BaseDock):
 
     def create_layout(self):
         form = QFormLayout()
-        self.table = QTableWidget()
-        self.table.setColumnCount(2)
-        self.table.setHorizontalHeaderLabels([ 'Key', 'Value' ])
-        self.table.verticalHeader().setDefaultSectionSize(25)
-        self.table.horizontalHeader().setResizeMode(QHeaderView.Stretch)
-        self.table.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
-        self.table.customContextMenuRequested.connect(self.context_menu)
-        self.table.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding))
+
+        self.model = VarsModel(self.data)
+
+        self.view = QTableView()
+        self.view.setModel(self.model)
+        self.view.horizontalHeader().setResizeMode(1, QHeaderView.Stretch)
+        self.view.verticalHeader().setDefaultSectionSize(22)
+        self.view.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.view.customContextMenuRequested.connect(self.context_menu)
+        self.view.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding))
+        self.view.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.view.setAlternatingRowColors(True)
 
         self.filter_combo = QComboBox()
         self.filter_combo.addItems(self.filters)
         self.filter_combo.currentIndexChanged.connect(self.filter_table)
 
         form.addRow('Filter:', self.filter_combo)
-        form.addRow(self.table)
+        form.addRow(self.view)
 
-        add_var_hbox = QHBoxLayout()
+        # Controls for adding/removing variables
+
         self.new_var_key = QLineEdit()
         self.new_var_value = QLineEdit()
-        add_var_hbox.addWidget(self.new_var_key)
-        add_var_hbox.addWidget(QLabel(':'))
-        add_var_hbox.addWidget(self.new_var_value)
         add_var_btn = QPushButton('Add')
         add_var_btn.clicked.connect(self.add_new_var)
-        add_var_hbox.addWidget(add_var_btn)
+        add_var_hbox = HBox(self.new_var_key, QLabel(':'), self.new_var_value, add_var_btn)
 
         self.del_var_key = QLineEdit()
         del_var_button = QPushButton('Delete')
         del_var_button.clicked.connect(self.remove_var)
-        del_var_hbox = QHBoxLayout()
-        del_var_hbox.addWidget(self.del_var_key)
-        del_var_hbox.addWidget(del_var_button)
+        del_var_hbox = HBox(self.del_var_key, del_var_button)
 
         self.save_button = QPushButton('Save')
         self.save_button.clicked.connect(self.save_variables)
@@ -78,34 +163,16 @@ class Variables(BaseDock):
 
     def filter_table(self):
         filter_str = str(self.filter_combo.currentText())
-        for i in range(self.table.rowCount()):
+        for i in range(self.model.rowCount()):
             if filter_str == 'None':
-                self.table.showRow(i)
+                self.view.showRow(i)
                 continue
 
-            item = self.table.item(i, 1)
-            if filter_str == item.data(QtCore.Qt.UserRole).toString():
-                self.table.showRow(i)
+            if filter_str == str(self.model.dataAt(i, 1, QtCore.Qt.UserRole).toString()):
+                self.view.showRow(i)
             else:
-                self.table.hideRow(i)
+                self.view.hideRow(i)
 
-    def classify_data(self, value):
-        """Determine what to categorize a value as."""
-        # If the value is not hex, assume text
-        try:
-            i = int(value, 16)
-        except ValueError:
-            return 'Text'
-
-        # See if it's a raw transaction.
-        try:
-            t = CTransaction.deserialize(value.decode('hex'))
-            return 'Raw Transaction'
-        except Exception:
-            pass
-
-        # Use the generic 'Hex' category if nothing else matches.
-        return 'Hex'
 
     def store_as_variable(self, value):
         """Prompt to store a value."""
@@ -122,19 +189,21 @@ class Variables(BaseDock):
     def get_key(self, key):
         """Get a value for a key.
 
-        Used by scriptedit for substituting values.
+        Used by scriptedit for highlighting variable keys.
         """
         return self.data.get(key)
 
     def set_key(self, key, value):
         """Store a new variable."""
-        self.data[key] = value
+        self.model.set_key(key, value)
         self.needsUpdate.emit()
 
     def remove_key(self, key):
         """Remove a key."""
-        if self.data.get(key):
-            del self.data[key]
+        try:
+            self.model.remove_key(key)
+        except ValueError:
+            self.status_message('No variable named "{}"'.format(key), True)
         self.needsUpdate.emit()
 
     def save_variables(self):
@@ -145,29 +214,22 @@ class Variables(BaseDock):
         menu = QMenu()
 
         def copy():
-            item = self.table.currentItem()
-            QApplication.clipboard().setText(str(item.text()))
+            index = self.view.currentIndex()
+            text = self.model.data(index).toString()
+            QApplication.clipboard().setText(str(text))
         menu.addAction('Copy', copy)
 
         def delete():
-            row = self.table.currentRow()
-            name = str(self.table.item(row, 0).text())
-            del self.data[name]
+            index = self.view.currentIndex()
+            row = index.row()
+            name = self.model.dataAt(row, 0).toString()
+            self.remove_key(str(name))
             self.needsUpdate.emit()
         menu.addAction('Delete', delete)
 
-        menu.exec_(self.table.viewport().mapToGlobal(position))
+        menu.exec_(self.view.viewport().mapToGlobal(position))
 
     def refresh_data(self):
-        self.table.clearContents()
-        self.table.setRowCount(0)
-        for k, v in self.data.items():
-            self.table.insertRow(0)
-            item_key = QTableWidgetItem(k)
-            item_value = QTableWidgetItem(str(v))
-            item_value.setData(QtCore.Qt.UserRole, self.classify_data(str(v)))
-            self.table.setItem(0, 0, item_key)
-            self.table.setItem(0, 1, item_value)
         self.filter_table()
 
     def add_new_var(self):
@@ -180,7 +242,7 @@ class Variables(BaseDock):
             self.status_message('Value must not be empty.', True)
             return
         self.set_key(k, v)
-        self.status_message('Set variable %s to %s'.format(k, v))
+        self.status_message('Set variable {} to {}'.format(k, v))
         self.new_var_key.clear()
         self.new_var_value.clear()
 

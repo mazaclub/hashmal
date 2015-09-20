42d666d27180250ccbcab542e9c254527abf063c
Kefkius <kefkius@mail.com>
2015-09-20 18:32:37 -0400

    Use class for ParamsPreset

--- hashmal_lib/core/chainparams.py
+++ hashmal_lib/core/chainparams.py
@@ -2,28 +2,53 @@ from collections import namedtuple
 
 import transaction
 
-ParamsPreset = namedtuple('ParamsPreset', ('name', 'tx_fields'))
+class ParamsPreset(object):
+    """Chainparams preset.
 
-BitcoinPreset = ParamsPreset('Bitcoin',
-           [('nVersion', b'<i', 4, 1),
+    Attributes:
+        - name (str): Identifier (e.g. 'Bitcoin').
+        - tx_fields (list): Transaction format. List of 4-tuples in the form:
+            (attribute_name, fmt, num_bytes, default), where:
+                - attribute_name (str): Name of the tx attribute.
+                - fmt (str): struct format string, or for special cases,
+                    'inputs', 'outputs', or 'bytes'.
+                - num_bytes (int): Number of bytes for deserializer to read,
+                - default: Default value.
+
+    """
+    def __init__(self, **kwargs):
+        self.name = ''
+        self.tx_fields = []
+
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+BitcoinPreset = ParamsPreset(
+        name='Bitcoin',
+        tx_fields=[('nVersion', b'<i', 4, 1),
             ('vin', 'inputs', None, None),
             ('vout', 'outputs', None, None),
-            ('nLockTime', b'<I', 4, 0)])
+            ('nLockTime', b'<I', 4, 0)]
+)
 
-ClamsPreset = ParamsPreset('Clams',
-           [('nVersion', b'<i', 4, 1),
+ClamsPreset = ParamsPreset(
+        name='Clams',
+        tx_fields=[('nVersion', b'<i', 4, 1),
             ('Timestamp', b'<i', 4, 0),
             ('vin', 'inputs', None, None),
             ('vout', 'outputs', None, None),
             ('nLockTime', b'<I', 4, 0),
-            ('ClamSpeech', 'bytes', None, b'')])
+            ('ClamSpeech', 'bytes', None, b'')]
+)
 
-PeercoinPreset = ParamsPreset('Peercoin',
-           [('nVersion', b'<i', 4, 1),
+PeercoinPreset = ParamsPreset(
+        name='Peercoin',
+        tx_fields=[('nVersion', b'<i', 4, 1),
             ('Timestamp', b'<i', 4, 0),
             ('vin', 'inputs', None, None),
             ('vout', 'outputs', None, None),
-            ('nLockTime', b'<I', 4, 0)])
+            ('nLockTime', b'<I', 4, 0)]
+)
 
 presets_list = [
         BitcoinPreset,
